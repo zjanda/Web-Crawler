49a4be54320eda01d6e04cf180b1b6bf794d1fbe
Zack Jandali <zjandali@uci.edu>
2021-04-21 17:59:08 +0300

    Added stuff to main.py and adjusted spacing in other files.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from time import time
+# This file is for algorithm testing only. The program is to be run from launch.py
+# import time as tm     # for delaying the program
+from time import time   # for speed checking algorithms
 import requests
 from bs4 import BeautifulSoup
 
 
-def is_scrapable(URL):
-    pass
+def is_valid_URL(URL):
+    return True     # change later
 
 
 def print_time(string, start):
@@ -14,9 +16,18 @@ def print_time(string, start):
 
 # Download the HTML document
 link = ['https://dataquestio.github.io/web-scraping-pages/simple.html', 'https://forecast.weather.gov/']
-page = requests.get(link[0])
-soup = BeautifulSoup(page.content, 'html.parser')
-p = soup.find_all('p')
+URL = link[1]
+if is_valid_URL(URL):
+    # Downloading the webpage
+    response = requests.get(URL)
+    if response.status_code == 200:
+        # Extracting the source code of the page
+        data = response.content     # can use .content (contents in bytes) or .text (contents in unicode)
+        # Creating BeautifulSoup Object
+        soup = BeautifulSoup(data, 'html.parser')
+        # Extract all <a> tags
+        tags = soup.find_all('a')
 
-for para in p:
-    print(para.get_text())
+        # Extracting urls from the attribute href in <a> tags.
+        for tag in tags:
+            print(tag.get('href'))
