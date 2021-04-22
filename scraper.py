83323b62745e30b1633de89f133d810d08d08fc6
ShaodongGu <52988477+ShaodongGu@users.noreply.github.com>
2021-04-22 14:28:41 +0800

    Added to scraper
    Make basic link extraction works.
    Added more implementation onto is_valid.

--- scraper.py
+++ scraper.py
@@ -1,19 +1,48 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+# TODO: later, record ans to all 4 questions
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation requred.
-    return list()
+    # TODO: only allow page with high text infomation contents (check piazza @125)
+
+    try:  # filter out page with no data
+        data = resp.raw_response.content # can use .content (contents in bytes) or .text
+    except:
+        return [];
+
+    data = resp.raw_response.content  # can use .content (contents in bytes) or .text
+    soup = BeautifulSoup(data, 'html.parser')
+
+    result = []
+    # Extract all <a> tags, get its 'href' value
+    for link in soup.find_all('a'):
+        result.append(link.get('href'));
+    return result
 
 def is_valid(url):
+    #TODO: 1. filter out traps
+    #      2. filter out sets of similar pages
+
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+
+        # restrict to only 5 allowed domains
+        if (parsed.hostname == None or
+            not ".ics.uci.edu" in parsed.hostname or
+            not ".cs.uci.edu" in parsed.hostname or
+            not ".informatics.uci.edu" in parsed.hostname or
+            not ".stat.uci.edu" in parsed.hostname or
+            not "today.uci.edu/department/information_computer_sciences" in parsed.hostname):
+            return False
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
